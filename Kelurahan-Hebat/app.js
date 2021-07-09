9d8423c8ed63fb78d853e88d8a6d4fa292645699
project-ant <s21810297@student.unklab.ac.id>
2021-07-09 11:14:28 +0800

    Update

--- Kelurahan-Hebat/app.js
+++ Kelurahan-Hebat/app.js
@@ -22,6 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//routing
+app.METHOD(PATH, HANDLER)
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
