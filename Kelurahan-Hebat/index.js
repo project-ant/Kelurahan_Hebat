b2cabd51d89d4917ba740f26a2609a8e9c37fc52
project-ant <s21810297@student.unklab.ac.id>
2021-07-12 10:12:58 +0800

    Hello World

---
+++ Kelurahan-Hebat/index.js
@@ -0,0 +1,16 @@
+// Hello World
+
+var express = require('../../');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
+
